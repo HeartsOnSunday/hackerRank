24519165e87df58e5a139a02956fe3ad4a176bef
Heartsonsunday <leahgdg@hotmail.com>
2019-03-24 13:58:38 -0400

    solved w/ parseFloat()

---
+++ 30daysofCode/2_operators.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+   /* 20
+    .20
+    1.20
+    mealcost * 1.20 */
+    let tip = parseFloat(tip_percent / 100);
+    console.log(tip);
+
+
+    
+   /* let totalCost = parseFloat((mealTipTax).toFixed(2))
+    console.log(totalCost);
+    */
+
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
