6d42cbd3b3064402fa9c181b8c02a80237d7476b
Heartsonsunday <leahgdg@hotmail.com>
2019-03-27 23:31:24 -0400

    write and test conditionals

--- 30daysofCode/03_conditionals.js
+++ 30daysofCode/03_conditionals.js
@@ -26,16 +26,21 @@ function readLine() {
 
 function main() {
     const N = parseInt(readLine(), 10);
-    console.log(N);
-    console.log(N);
-    if (N % 2 == 0) {
-       // console.log("N is even:", N);
+    //console.log(N);
+    if (N % 2 == 0 && N > 20) {
+        console.log("N is even and greater than 20:", N);
+    } else if (N % 2 == 0 && N <= 20 && N >= 6){
+console.log("N is even, greater than 6 and less than 20", N);
+    } else if (N % 2 == 0 && N <= 5 && N >=2) {
+console.log("N is even, less than or equal to 5 and greater than or equal to 2");
     } else if (!N % 2 == 0) {
-        console.log("Weird");
+        console.log("Weird", N);
     }
 }
 
 //Test N w console.log();
 //sort N if(even) or if (odd) w conditionals using % and 2
 //test with console.log();
-//odd numbers will print 'Weird' w console.log()
+//test conditional to print 'Weird' w console.log() for even numers over 20
+//test conditional to sort even numbers greater than 6 and less than 20
+//test conditional to sort even numbers less than or equal to 5 and greater than or equal to 2
